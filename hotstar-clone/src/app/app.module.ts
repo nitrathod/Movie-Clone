33f3cb43bc7e2d06bdd257cb9b9e85a3febb8a06
Nitin Rathod <55508648+nitrathod@users.noreply.github.com>
2022-03-09 16:08:19 +0530

    Feat: Added home and movies components

--- hotstar-clone/src/app/app.module.ts
+++ hotstar-clone/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
