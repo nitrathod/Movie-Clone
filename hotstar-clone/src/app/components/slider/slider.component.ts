7f6e8fa762e435fab615f8d9647309c1c020d044
Nitin Rathod <55508648+nitrathod@users.noreply.github.com>
2022-03-11 16:28:26 +0530

    Style: slider updated with counter and animation

--- hotstar-clone/src/app/components/slider/slider.component.ts
+++ hotstar-clone/src/app/components/slider/slider.component.ts
@@ -10,15 +10,20 @@ import { Movie } from '../../models/movie';
   animations: [
     trigger('slideFade', [
       state('void', style({ opacity: 0 })),
-      transition('void => *', [animate('1s')]),
-      transition('* => void', [animate('500ms')])
+      transition('void <=> *', [animate('1s')])
+      // transition('* => void', [animate('500ms')])
     ])
   ]
 })
 export class SliderComponent implements OnInit {
   @Input() items: Movie[] = [];
 
+  currentSlideIndex = 0;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentSlideIndex = ++this.currentSlideIndex % this.items.length;
+    }, 5000);
+  }
 }
