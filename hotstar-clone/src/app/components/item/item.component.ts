0dd0ea09942906f7b41e25eb54a6a4383a33f70d
Nitin Rathod <55508648+nitrathod@users.noreply.github.com>
2022-03-13 08:06:25 +0530

    Update: Displaying movie data under item component

--- hotstar-clone/src/app/components/item/item.component.ts
+++ hotstar-clone/src/app/components/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from '../../models/movie';
 
 @Component({
   selector: 'item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.scss']
 })
 export class ItemComponent implements OnInit {
+  @Input() itemData: Movie | null = null;
+
   constructor() {}
 
   ngOnInit(): void {}
--- hotstar-clone/src/app/components/items-banner/items-banner.component.ts
+++ hotstar-clone/src/app/components/items-banner/items-banner.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Movie } from '../../models/movie';
 
 @Component({
   selector: 'items-banner',
   templateUrl: './items-banner.component.html',
   styleUrls: ['./items-banner.component.scss']
 })
-export class ItemsBannerComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class ItemsBannerComponent {
+  @Input() items: Movie[] = [];
 }
