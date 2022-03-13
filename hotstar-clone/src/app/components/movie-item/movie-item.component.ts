4003c7d868401590c04410722fdab859a60552b8
Nitin Rathod <55508648+nitrathod@users.noreply.github.com>
2022-03-13 06:52:49 +0530

    Feat: Movie-item component added, styled

--- hotstar-clone/src/app/components/movie-item/movie-item.component.ts
+++ hotstar-clone/src/app/components/movie-item/movie-item.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-movie-item',
+  selector: 'movie-item',
   templateUrl: './movie-item.component.html',
   styleUrls: ['./movie-item.component.scss']
 })
 export class MovieItemComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
