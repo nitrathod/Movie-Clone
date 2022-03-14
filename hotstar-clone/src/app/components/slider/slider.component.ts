467a9c632e9a508885a5c2d727019285b4694a5f
Nitin Rathod <55508648+nitrathod@users.noreply.github.com>
2022-03-14 22:06:49 +0530

    Feat: Displaying slider as a banner on movie details page

--- hotstar-clone/src/app/components/slider/slider.component.ts
+++ hotstar-clone/src/app/components/slider/slider.component.ts
@@ -18,14 +18,17 @@ import { Movie } from '../../models/movie';
 })
 export class SliderComponent implements OnInit {
   @Input() items: Movie[] = [];
+  @Input() isBanner = false;
 
   currentSlideIndex = 0;
 
   readonly imagesSizes = IMAGES_SIZES;
 
   ngOnInit(): void {
-    setInterval(() => {
-      this.currentSlideIndex = ++this.currentSlideIndex % this.items.length;
-    }, 5000);
+    if (!this.isBanner) {
+      setInterval(() => {
+        this.currentSlideIndex = ++this.currentSlideIndex % this.items.length;
+      }, 5000);
+    }
   }
 }
